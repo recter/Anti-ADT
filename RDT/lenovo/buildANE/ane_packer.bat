1ad92ba52025031d194b2b9d21da0f7534801c1e
Rect <rectvv@gmail.com>
2014-01-06 10:53:13 +0800

    update root file

--- RDT/lenovo/buildANE/ane_packer.bat
+++ RDT/lenovo/buildANE/ane_packer.bat
@@ -1,1 +1,1 @@
-"D:\Flashbuilder\Adobe Flash Builder 4.6\sdks\AIR3.5\bin\adt" -package -storetype PKCS12 -keystore rect.p12 -storepass 1234 -target ane com.lenovo.ane extension.xml -swc *.swc -platform Android-ARM -C Android-ARM . 
+"D:\fb\Adobe Flash Builder 4.6\sdks\AIR3.5\bin\adt" -package -storetype PKCS12 -keystore rect.p12 -storepass 1234 -target ane com.lenovo.ane extension.xml -swc *.swc -platform Android-ARM -C Android-ARM . 
